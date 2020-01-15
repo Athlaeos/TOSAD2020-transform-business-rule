2cc151ffbf241bf64e9147cf3587bb1dce40ec5c
jobko <jobkoppenol@gmail.com>
2020-01-15 12:17:38 +0100

    firstapp commit

---
+++ src/main/java/tocba2020/transformbusinessrule/App.java
@@ -0,0 +1,13 @@
+package tocba2020.transformbusinessrule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
