c2c02ef5e5fb25ba35eb5002faa53ccd91c19c8c
donneh4 <donny@i-aspect.nl>
2020-01-23 14:11:44 +0100

    Create Rule from JSON

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,27 @@
 import application.serviceInterfaces.Controller;
 import domain.businessRules.AttributeRangeRule;
 
+import java.io.IOException;
+
 public class App {
     public static void main( String[] args ){
+        String defineInput = "{" +
+                "\"notter\": true," +
+                "\"operatorValue\": \"Test1\"," +
+                "\"compareValue\": \"10\"," +
+                "\"compareValue2\": \"100\"," +
+                "\"name\": \"between10And100\"," +
+                "\"description\": \"check if value is between 10 and 100\"," +
+                "\"when\": \"before\"," +
+                "\"event\": \"update,insert\"," +
+                "\"forEachRow\": false," +
+                "\"ErrorMessage\": \"Error message\"," +
+                "\"tableName\": \"Tabelnaam\"" +
+                "}";
         Controller controller = new Controller();
-        AttributeRangeRule attributeRangeRule = controller.createAttributeRangeRule(true,"test1","test2","test3","test4","test5","test6","test7",true,"test8","test9");
+        AttributeRangeRule attributeRangeRule = null;
+        attributeRangeRule = controller.mapToRangeRule(defineInput);
+//        AttributeRangeRule attributeRangeRule = controller.createAttributeRangeRule(true,"test1","test2","test3","test4","test5","test6","test7",true,"test8","test9");
         System.out.println(controller.generateAttributeRangeRuleTriggerQuery(attributeRangeRule));
         System.out.println(controller.generateAttributeRangeRuleConstraintQuery(attributeRangeRule));
     }
--- src/main/java/application/serviceInterfaces/Controller.java
+++ src/main/java/application/serviceInterfaces/Controller.java
@@ -1,6 +1,13 @@
 package application.serviceInterfaces;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.businessRules.AttributeRangeRule;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class Controller {
     public Controller(){
@@ -22,5 +29,31 @@ public class Controller {
 
     public String generateAttributeRangeRuleConstraintQuery(AttributeRangeRule attributeRangeRule){
         return generatorController().generateBusinessRuleConstraintrQuery(attributeRangeRule);
+    }
+
+    public AttributeRangeRule mapToRangeRule(String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Map value = mapper.readValue(json, Map.class);
+            System.out.println(value);
+            AttributeRangeRule attributeRangeRule = new AttributeRangeRule(
+                    Boolean.parseBoolean(value.get("notter").toString()),
+                    value.get("operatorValue").toString(),
+                    value.get("compareValue").toString(),
+                    value.get("compareValue2").toString(),
+                    value.get("name").toString(),
+                    value.get("description").toString(),
+                    value.get("when").toString(),
+                    value.get("event").toString(),
+                    Boolean.parseBoolean(value.get("forEachRow").toString()),
+                    value.get("ErrorMessage").toString(),
+                    value.get("tableName").toString()
+            );
+
+            return attributeRangeRule;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
